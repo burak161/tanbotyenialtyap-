fe59a271013186206f620614ea40ca869ebdf926
burak <65681772+burak161@users.noreply.github.com>
2022-06-09 20:34:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on("message", message => {
 client.on("ready", () => {
   console.log(`Bütün komutlar başarıyla yüklendi!`);
   client.user.setStatus("dnd");
-  client.user.setActivity('-pirtyy | -yardım');
+  client.user.setActivity('edited by pїятчч | -yardım');
 })
 
 
